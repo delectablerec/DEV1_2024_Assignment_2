ac1117c696028b107ecdc94fcf48d3b86d4dabd4
Pozzame <pozzame@gmail.com>
2024-11-12 17:04:09 +0100

    Tolti

--- Models/ViewModels/ProfiloAdminViewModel.cs
+++ Models/ViewModels/ProfiloAdminViewModel.cs
@@ -3,5 +3,4 @@ public class ProfiloAdminViewModel
     public List<Cliente> Clienti { get; set; }
     public List<Ordine> Ordini { get; set; }
     public List<Prodotto> Prodotti { get; set; }
-    public string NomeUtente { get; set; } // Per l'integrazione con Identity
 }
--- Models/ViewModels/ProfiloUtenteViewModel.cs
+++ Models/ViewModels/ProfiloUtenteViewModel.cs
@@ -3,6 +3,4 @@ public class ProfiloUtenteViewModel
     public Cliente Cliente { get; set; }
     public List<Ordine> Ordini { get; set; }
     public List<Orologio> Wishlist { get; set; }
-    public string NomeUtente { get; set; } // Per l'integrazione con Identity
-    public string Email { get; set; } // Per l'integrazione con Identity
 }
