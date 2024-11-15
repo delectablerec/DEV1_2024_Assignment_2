251c18747ca437d5829209570b29b0b57ecd603d
Pozzame <pozzame@gmail.com>
2024-11-15 15:54:19 +0100

    Ok

--- Models/ViewModels/CarrelloViewModel.cs
+++ Models/ViewModels/CarrelloViewModel.cs
@@ -1,3 +1,6 @@
 public class CarrelloViewModel
 {
+    public List<Orologio> Carrello { get; set; } = new List<Orologio>();
+    public decimal Totale { get; set; } = 0;
+    public int Quantita { get; set; } = 0;
 }
