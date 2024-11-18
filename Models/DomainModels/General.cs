15b8f9c38e4c6e443c2620c65cc62500a08b4766
Ginevra9684 <ginevravallebona98@gmail.com>
2024-11-19 00:59:39 +0100

    filtri prodotti funzionanti

--- Models/DomainModels/General.cs
+++ Models/DomainModels/General.cs
@@ -1,7 +1,5 @@
-using System.ComponentModel.DataAnnotations;
 public abstract class General
 {
     public virtual int Id { get; set; }
-    [Required(ErrorMessage = "Il campo Nome è obbligatorio.")]
     public virtual string Nome { get; set; }
 }
