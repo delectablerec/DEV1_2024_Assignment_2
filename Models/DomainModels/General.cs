b836a75f35688280721e63a7977ff977e9cd4622
Ginevra9684 <ginevravallebona98@gmail.com>
2024-11-18 18:00:34 +0100

    messaggi errore da finire

--- Models/DomainModels/General.cs
+++ Models/DomainModels/General.cs
@@ -1,6 +1,7 @@
+using System.ComponentModel.DataAnnotations;
 public abstract class General
 {
     public virtual int Id { get; set; }
+    [Required(ErrorMessage = "Il campo Nome è obbligatorio.")]
     public virtual string Nome { get; set; }
-    
 }
