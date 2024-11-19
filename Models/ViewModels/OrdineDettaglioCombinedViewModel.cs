b57770b20eb8d3dc95df2ee81848aef9d7959851
Neomatrix0 <danimusci@yahoo.it>
2024-11-19 07:37:22 +0100

    ordini fix viewmodel

--- Models/ViewModels/OrdineDettaglioCombinedViewModel.cs
+++ Models/ViewModels/OrdineDettaglioCombinedViewModel.cs
@@ -1,8 +1,7 @@
-namespace OrdiniTest.ViewModels
-{
+
     public class OrdineDettaglioCombinedViewModel
     {
         public DettaglioOrdineViewModel OrdineDettaglio { get; set; } = null!;
         public List<DettaglioOrdineProdottoViewModel> ProdottiDettaglio { get; set; } = new List<DettaglioOrdineProdottoViewModel>();
     }
-}
+
