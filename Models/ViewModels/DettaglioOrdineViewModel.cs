f33b9df4a9b9c2af9f75a637d1b9ab8cdbf823ac
Neomatrix0 <danimusci@yahoo.it>
2024-11-19 07:26:36 +0100

    ordini fix correzione

--- Models/ViewModels/DettaglioOrdineViewModel.cs
+++ Models/ViewModels/DettaglioOrdineViewModel.cs
@@ -1,4 +1,3 @@
-
 public class DettaglioOrdineViewModel
 {
     public int OrdineId { get; set; }
@@ -14,22 +13,3 @@ public class DettaglioOrdineViewModel
     public decimal Subtotale { get; set; }
     public decimal Totale { get; set; }
 }
-
-/*public class DettaglioOrdineViewModel
-{
-    public int OrdineId { get; set; } // ID dell'ordine
-    public string NomeOrdine { get; set; } // Nome o numero identificativo dell'ordine
-    public string ClienteNome { get; set; } // Nome del cliente
-    //public string ClienteTelefono { get; set; } // Telefono del cliente
-    public string IndirizzoSpedizione { get; set; } // Indirizzo di spedizione
-    public string MetodoPagamento { get; set; } // Metodo di pagamento
-    public string TipoSpedizione { get; set; } // Tipo di spedizione
-    public decimal CostoSpedizione { get; set; } // Costo di spedizione
-    public string StatoOrdine { get; set; } // Stato dell'ordine (enum convertito in stringa)
-    public DateTime DataAcquisto { get; set; } // Data di acquisto
-    public List<Orologio> Prodotti { get; set; } // Lista dei prodotti nell'ordine
-     public CarrelloViewModel Carrello { get; set; } = new CarrelloViewModel();
-
-     public decimal Subtotale { get; set; }
-     public decimal Totale {get;set;}
-}*/
